149abcef9eab5ddac066469205147cb0340f775a
mcgrathricam <mcgrathricamoresparks2mvrs@hotmail.com>
2024-09-20 15:41:09 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("08bbb387170874b8805863799662f9835b99485eace4c4d3a009ec8e62a9dd83", "08bbb387170874b8805863799662f9835b99485eace4c4d3a009ec8e62a9dd83") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
